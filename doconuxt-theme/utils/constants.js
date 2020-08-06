058dacc09b88e159a9dc241c87418c331b675b67
Aaron Choo <choozy.aaron@gmail.com>
2020-08-06 11:14:12 +0800

    fix: remove `/` prefix from path of user content

--- doconuxt-theme/utils/constants.js
+++ doconuxt-theme/utils/constants.js
@@ -1,5 +1,6 @@
-const USER_CONFIG_DIR = "/_config";
-
-const SIDENAV_CONFIG_PATH = USER_CONFIG_DIR + "/sidenav";
+//! path to content must start without `/` prefix
+const USER_CONFIG_PATH = "_config";
+const USER_CONFIG_DIR = "/" + USER_CONFIG_PATH;
+const SIDENAV_CONFIG_PATH = USER_CONFIG_PATH + "/sidenav";
 
 export { USER_CONFIG_DIR, SIDENAV_CONFIG_PATH };
